c61a895ab345eca06bec74207c9d5280078eda92
Taaha Mohsin <taaha.b.mohsin@vanderbilt.edu>
2020-07-05 15:18:26 -0400

    Fixed schema init

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var ratingsRouter = require('./routes/ratings');
+var connection = require('./dbConnection');
 
 var app = express();
 
@@ -27,6 +28,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
+
+connection.query('CREATE TABLE IF NOT EXISTS ratings (id int NOT NULL AUTO_INCREMENT, title varchar(45) NOT NULL, description varchar(255) DEFAULT NULL, value int NOT NULL, PRIMARY KEY (id))');
+connection.query('CREATE TABLE IF NOT EXISTS users (id int NOT NULL AUTO_INCREMENT, first_name varchar(45) NOT NULL, last_name varchar(45) NOT NULL, score int NOT NULL DEFAULT 0, img_url varchar(45) DEFAULT NULL, PRIMARY KEY (id))');
+connection.query(`INSERT INTO \`${process.env.DB_NAME}\`.ratings (title, description, value) VALUES ('Not bad', 'This was your average bad joke', 1)`);
+connection.query(`INSERT INTO \`${process.env.DB_NAME}\`.ratings (title, description, value) VALUES ('Make me suffer', 'This joke made you question your life choices up to this point', 2)`);
+connection.query(`INSERT INTO \`${process.env.DB_NAME}\`.ratings (title, description, value) VALUES ('Oh my fucking god', 'This bad joke made you cringe on a spiritual level', 3)`);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
