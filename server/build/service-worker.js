75bd5776d3afb73f03518b29d5dd7704ef5aa60c
Taaha Mohsin <taaha.b.mohsin@vanderbilt.edu>
2020-07-05 22:55:53 -0400

    Added delete func for users

--- server/build/service-worker.js
+++ server/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.023fc742ae37c36ab9668f1e88541c62.js"
+  "/precache-manifest.717a89d191bac87db02865e3941f1ae3.js"
 );
 
 self.addEventListener('message', (event) => {
